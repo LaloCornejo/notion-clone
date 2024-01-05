8f8370699b088f4914351938e2a9ef5893b9af00
L Æ L Ö <lalocornejoofficial@gmail.com>
2024-01-04 21:37:11 -0600

    Refactor layout component

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -1,41 +1,41 @@
 "use client";
 
-import { Spinner } from "@/components/spinner";
 import { useConvexAuth } from "convex/react";
-import { redirect } from "next/dist/server/api-utils";
+import { redirect } from "next/navigation";
 
-import { Navigation } from "./_components/navigation";
+import { Spinner } from "@/components/spinner";
 import { SearchCommand } from "@/components/search-command";
 
+import { Navigation } from "./_components/navigation";
+
 const MainLayout = ({
-    children
+  children
 }: {
-    children:React.ReactNode;
+  children: React.ReactNode;
 }) => {
+  const { isAuthenticated, isLoading } = useConvexAuth();
 
-    const { isAuthenticated, isLoading } = useConvexAuth();
+  if (isLoading) {
+    return (
+      <div className="h-full flex items-center justify-center">
+        <Spinner size="lg" />
+      </div>
+    );
+  }
 
-    if( isLoading ){
-        return(
-            <div className="h-full flex items-center justify-center">
-                <Spinner size="lg"/>
-            </div>
-        );
-    }
+  if (!isAuthenticated) {
+    return redirect("/");
+  }
 
-    if( !isAuthenticated ) {
-        return redirect("/");
-    }
-
-    return ( 
-        <div className="h-full flex dark:bg-[#1f1f1f]">
-            <Navigation />
-            <main className="flex-1 h-full overflow-auto">
-                <SearchCommand />
-                {children}
-            </main>
-        </div>
-     );
+  return ( 
+    <div className="h-full flex dark:bg-[#1F1F1F]">
+      <Navigation />
+      <main className="flex-1 h-full overflow-y-auto">
+        <SearchCommand />
+        {children}
+      </main>
+    </div>
+   );
 }
  
 export default MainLayout;
