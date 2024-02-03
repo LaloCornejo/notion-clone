d206d0c1bd7b6b3c8d1b06e202b1c01dc0930e9e
L Æ L Ö <lalocornejoofficial@gmail.com>
2024-02-03 13:42:20 -0600

    Add @vercel/speed-insights package

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,6 +9,7 @@ import { ThemeProvider } from "@/components/providers/theme";
 import { ConvexClientProvider } from "@/components/providers/convex-provider";
 import { ModalProvider } from "@/components/providers/modal-provider";
 import { EdgeStoreProvider } from "@/lib/edgestore";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 const inter = Inter({ subsets: ["latin"] });
 
